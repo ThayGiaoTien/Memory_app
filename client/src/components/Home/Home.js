15431dae2bb7da173903c8f9d824973bd85baaa0
ThayGiaoTien <thelaststanding97@gmail.com>
2021-05-21 14:52:20 +0300

    Update v4.2: Add search button by searchQuery or tags, and dispatch them to reducers. Now we need to handle it in backend end then display results in frontend

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,29 +1,83 @@
 import React, { useState, useEffect } from 'react';
-import {Container, Grow, Grid, Paper} from '@material-ui/core';
+import {Container, Grow, Grid, Paper, AppBar, TextField, Button} from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import Paginate from '../Pagination';
-import {getPosts} from '../../actions/posts';
+import {getPosts, getPostsBySearch} from '../../actions/posts';
 import { useDispatch } from 'react-redux';
+import {useLocation, useHistory} from 'react-router-dom';
+import App from '../../App';
+import useStyles from './styles';
+import ChipInput from 'material-ui-chip-input';
 
-
+function useQuery(){
+    return new URLSearchParams(useLocation().search);
+};
 const Home= ()=>{
     const [currentId, setCurrentId]= useState(0);
     const dispatch= useDispatch();
+    const query= useQuery();
+    const history= useHistory();
+    const page= query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const classes= useStyles();
+    const [search, setSearch]= useState();
+    const [tags, setTags]= useState([]);
+
     useEffect(()=>{
         dispatch(getPosts());
     },[currentId, dispatch]);
+    
+    const searchPost=()=>{
+        if(search.trim()){
+            //dispatch=> fetch search posts
+            dispatch(getPostsBySearch({search, tags: tags.join(',')}));
+        } else {
+            history.push('/');
+        }
+    };
+    const handleKeyPress=(e)=>{                 //13 means enter
+        if(e.keyCode===13) {
+            //search post
+            searchPost();
+        }
+    };  
+    const handleAdd= (tag)=>{setTags([...tags, tag])};
+    const handleDelete=(tagToDelete)=>{setTags(tags.filter((tag)=>tag!==tagToDelete))};
+
+
 
     return (
         <Grow in >
-            <Container>
-                <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        < Posts setCurrentId= {setCurrentId} />
+            <Container maxWidth='xl'>
+                <Grid container justify='space-between' alignItems='stretch' spacing={3} className={classes.gridContainer}>
+                    <Grid item xs={12} sm={6} md={9}>
+                        <Posts setCurrentId= {setCurrentId} />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar className={classes.appBarSearch} color= 'inherit' position='static'>
+                            <TextField 
+                                name = "search"
+                                variant="outlined"
+                                label="Search Memories"
+                                fullWidth
+                                onKeyPress={handleKeyPress}
+                                value={search}
+                                onchange={(e)=>{setSearch(e.target.value)}}
+                            />
+                            <ChipInput 
+                                style={{margin:'10px 0'}}
+                                value={tags}
+                                onAdd={handleAdd}
+                                onDelete={handleDelete}
+                                label='Search Tags'
+                                variant='outlined'
+                            />
+                            <Button className={classes.searchButton} variant ='contained' color='primary' onClick={searchPost}>Search</Button>
+                        </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        <Paper >
+                        <Paper elevation={6}>
                             <Paginate/>
                         </Paper>
                     </Grid>
